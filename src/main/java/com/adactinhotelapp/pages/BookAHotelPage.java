dc8641a39d53ffa6bdafa994956682893e633dfc
raobkkraju <raobkkraju@gmail.com>
2023-02-15 03:15:58 +0530

    Updated BookAHotel page

--- src/main/java/com/adactinhotelapp/pages/BookAHotelPage.java
+++ src/main/java/com/adactinhotelapp/pages/BookAHotelPage.java
@@ -43,49 +43,87 @@ WebElement hotel_FirstName_Txt;
 
 @FindBy(xpath="//input[@id='last_name']")
 WebElement hotel_LastName_Txt;
-
 	
 @FindBy(xpath="//textarea[@id='address']")
 WebElement hotel_BillingAddress;
 
-
 @FindBy(xpath="//input[@id='cc_num']")
 WebElement hotel_CreditCardNum;
-
 
 @FindBy(xpath="//select[@id='cc_type']")
 WebElement hotel_CreditCardType;
 
-
 @FindBy(xpath="//select[@id='cc_exp_month']")
 WebElement hotel_ExpiryMonth; 	
-
 
 @FindBy(xpath="//select[@id='cc_exp_year']")
 WebElement hotel_ExpiryYear;
 
-
 @FindBy(xpath="//input[@id='cc_cvv']")
 WebElement hotel_Cc_CVV;
-
 
 @FindBy(xpath="//input[@id='book_now']")
 WebElement hotel_BookNow_Btn;
 
+public void hotel_nameDisplay_Txt(String text)
+{
+	verifyEquals((hotel_nameDisplay_Txt.getText()),text);	
+}
+
+public void hotel_LocationDisplay_Txt(String text)
+{
+	verifyEquals((hotel_LocationDisplay_Txt.getText()),text);	
+}
+
+public void hotel_RoomDisplay_Txt(String text)
+{
+	verifyEquals((hotel_RoomDisplay_Txt.getText()),text);	
+}
+
+public void hotel_NumOfRooms_Txt(String text)
+{
+	verifyEquals((hotel_NumOfRooms_Txt.getText()),text);	
+}
+
+public void hotel_TotalDays_Txt(String text)
+{
+	verifyEquals((hotel_TotalDays_Txt.getText()),text);	
+}
+
+public void hotel_PricePerNight_Txt(String text)
+{
+	verifyEquals((hotel_PricePerNight_Txt.getText()),text);	
+}
+public void hotel_TotalPrice_Txt(String text)
+{
+	verifyEquals((hotel_TotalPrice_Txt.getText()),text);	
+}
+
+public void hotel_PriceGST_Txt(String text)
+{
+	verifyEquals((hotel_PriceGST_Txt.getText()),text);	
+}
+
+public void hotel_FinalBilledPrice_Txt(String text)
+{
+	verifyEquals((hotel_FinalBilledPrice_Txt.getText()),text);	
+}
 
 public void hotel_FirstName_Txt(String text)
 {
-	type(hotel_FirstName_Txt,text);
+	verifyEquals((hotel_FirstName_Txt.getText()),text);
+	
 }
 
 public void hotel_LastName_Txt(String text)
 {
-	type(hotel_LastName_Txt,text);
+	verifyEquals((hotel_LastName_Txt.getText()),text);	
 }
+
 
 public void hotel_BillingAddress(String text)
 {
-	type(hotel_BillingAddress,text );
+		type(hotel_BillingAddress,text );
 }
 
 public void hotel_CreditCardNum(String text)
