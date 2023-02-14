94e794f206f000eed78610b094921ea49f15a8de
raobkkraju <raobkkraju@gmail.com>
2023-02-15 01:05:57 +0530

    Added 'BookAHotelPage'

---
+++ src/main/java/com/adactinhotelapp/pages/BookAHotelPage.java
@@ -0,0 +1,125 @@
+package com.adactinhotelapp.pages;
+
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class BookAHotelPage extends Page {
+
+public BookAHotelPage()
+{
+	PageFactory.initElements(driver, this);
+}
+
+@FindBy(xpath="//input[@id='hotel_name_dis']")
+WebElement hotel_nameDisplay_Txt;
+		
+@FindBy(xpath="//input[@id='location_dis']")
+WebElement hotel_LocationDisplay_Txt;
+	
+@FindBy(xpath="//input[@id='room_type_dis']")
+WebElement hotel_RoomDisplay_Txt;
+
+@FindBy(xpath="//input[@id='room_num_dis']")
+WebElement hotel_NumOfRooms_Txt;
+
+@FindBy(xpath="//input[@id='total_days_dis']")
+WebElement hotel_TotalDays_Txt;
+
+@FindBy(xpath="//input[@id='price_night_dis']")
+WebElement hotel_PricePerNight_Txt;
+
+@FindBy(xpath="//input[@id='price_night_dis']")
+WebElement hotel_TotalPrice_Txt;
+
+@FindBy(xpath="//input[@id='gst_dis']")
+WebElement hotel_PriceGST_Txt;
+
+@FindBy(xpath="//input[@id='final_price_dis']")
+WebElement hotel_FinalBilledPrice_Txt;
+
+@FindBy(xpath="//input[@id='first_name']")
+WebElement hotel_FirstName_Txt;
+
+@FindBy(xpath="//input[@id='last_name']")
+WebElement hotel_LastName_Txt;
+
+	
+@FindBy(xpath="//textarea[@id='address']")
+WebElement hotel_BillingAddress;
+
+
+@FindBy(xpath="//input[@id='cc_num']")
+WebElement hotel_CreditCardNum;
+
+
+@FindBy(xpath="//select[@id='cc_type']")
+WebElement hotel_CreditCardType;
+
+
+@FindBy(xpath="//select[@id='cc_exp_month']")
+WebElement hotel_ExpiryMonth; 	
+
+
+@FindBy(xpath="//select[@id='cc_exp_year']")
+WebElement hotel_ExpiryYear;
+
+
+@FindBy(xpath="//input[@id='cc_cvv']")
+WebElement hotel_Cc_CVV;
+
+
+@FindBy(xpath="//input[@id='book_now']")
+WebElement hotel_BookNow_Btn;
+
+
+public void hotel_FirstName_Txt(String text)
+{
+	type(hotel_FirstName_Txt,text);
+}
+
+public void hotel_LastName_Txt(String text)
+{
+	type(hotel_LastName_Txt,text);
+}
+
+public void hotel_BillingAddress(String text)
+{
+	type(hotel_BillingAddress,text );
+}
+
+public void hotel_CreditCardNum(String text)
+{
+	type(hotel_CreditCardNum,text);
+}
+
+public void hotel_CreditCardType(String text)
+{
+	select(hotel_CreditCardType,text);
+}
+
+public void hotel_ExpiryMonth(String text)
+{
+	select(hotel_ExpiryMonth,text);
+}
+
+public void hotel_ExpiryYear(String text)
+{
+	select(hotel_ExpiryYear,text);
+}
+
+public void hotel_Cc_CVV(String text)
+{
+	type(hotel_Cc_CVV,text);
+}
+
+public void hotel_BookNow_Btn(WebElement hotel_BookNow_Btn)
+{
+	click(hotel_BookNow_Btn);
+}
+
+
+}
+
+
+ 
